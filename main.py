7dd3b10d333e208e516798d9bc6943d221a5c56b
dwissaaj <dwis.buntelman123@gmail.com>
2021-10-13 16:59:42 +0700

    changed for mining

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import openpyxl
 
 params = {
     "engine": "google_maps",
-    "q": "Pos Indonesia Pusat",
+    "q": "Kantor Pusat J&T",
     "hl": "id",
     "type": "search",
-    "location": "Bandung",
+    "location": "North Jakarta",
     "api_key": "1c33c59f4cb1d26dd31598098acc27c528873a12c11c0f583d6cdcfec5fa6d1d"
 }
 
@@ -17,4 +17,3 @@ results = search.get_dict()
 
 local_results = results['local_results']
 data = pd.DataFrame(local_results)
-data.to_excel("pos indonesia.xlsx")
